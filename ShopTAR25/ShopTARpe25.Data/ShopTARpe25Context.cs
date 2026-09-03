5fa60c5ffedd828634b8cef0eabd0cbba467c5e4
Opilane Techno+TLN <jelizaveta890@gmail.com>
2026-09-03 14:14:51 +0300

    -

--- ShopTAR25/ShopTARpe25.Data/ShopTARpe25Context.cs
+++ ShopTAR25/ShopTARpe25.Data/ShopTARpe25Context.cs
@@ -8,7 +8,12 @@ namespace ShopTARpe25.Data
     //Teha selle classist DbContext, et saaks andmebaasi kasutada
     public class ShopTARpe25Context : DbContext
     {
-
-
+        public ShopTARpe25Context(DbContextOptions<ShopTARpe25Context> options) 
+            : base(options)
+        { 
+        }
+        //teha DbSet, et saaks andmebaasi kasutada
+        //teha Core projekti alla Domaini nimega kaust ja sinna class 
+        //nimega Spaceship
     }
 }
