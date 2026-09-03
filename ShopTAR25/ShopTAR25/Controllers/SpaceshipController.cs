8dfa70384374c39151633b89cd64d92d0a4fc1d4
Opilane Techno+TLN <jelizaveta890@gmail.com>
2026-09-03 13:42:21 +0300

    DbContext

---
+++ ShopTAR25/ShopTAR25/Controllers/SpaceshipController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopTAR25.Controllers
+{
+    public class SpaceshipController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Teha Data projekti alla ShopTARpe25Context nimega class 
+    }
+}
---
+++ ShopTAR25/ShopTARpe25.Data/ShopTARpe25Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopTARpe25.Data
+{
+    //Teha selle classist DbContext, et saaks andmebaasi kasutada
+    public class ShopTARpe25Context : DbContext
+    {
+
+
+    }
+}
