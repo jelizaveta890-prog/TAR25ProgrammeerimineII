04ab2ee21a919c269d8ced718c9cc14cbe282bec
Opilane Techno+TLN <jelizaveta890@gmail.com>
2026-09-07 14:10:18 +0300

    Context

--- ShopTAR25/ShopTARpe25.Data/ShopTARpe25Context.cs
+++ ShopTAR25/ShopTARpe25.Data/ShopTARpe25Context.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using ShopTARpe25.Core.Domain;
+
 
 namespace ShopTARpe25.Data
 {
@@ -13,7 +12,8 @@ namespace ShopTARpe25.Data
         { 
         }
         //teha DbSet, et saaks andmebaasi kasutada
-        //teha Core projekti alla Domaini nimega kaust ja sinna class 
         //nimega Spaceship
+        public DbSet<Spaceship> Spaceships { get; set; }
+
     }
 }
