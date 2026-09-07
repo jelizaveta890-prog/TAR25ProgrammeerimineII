f09bf4b927c597133867dc92fd6b5fd65245d29f
Opilane Techno+TLN <jelizaveta890@gmail.com>
2026-09-07 14:10:31 +0300

    Context

--- ShopTAR25/ShopTARpe25ApplicationServices/Services/SpaceshipServices.cs
+++ ShopTAR25/ShopTARpe25ApplicationServices/Services/SpaceshipServices.cs
@@ -1,11 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ShopTARpe25.Core.Domain;
+using ShopTARpe25.Core.Dto;
+using ShopTARpe25.Data;
+
 
 namespace ShopTARpe25.ApplicationServices.Services
 {
+
     public class SpaceshipServices
     {
 
+        private readonly ShopTARpe25Context _context;
+
+        public SpaceshipServices
+           (
+             ShopTARpe25Context context
+           )
+        {
+            _context = context;
+        }
+
+
+        public async Task<Spaceship> Create(SpaceshipDto dto)
+        {
+            Spaceship domain = new();
+
+            domain.Id = dto.Id;
+            domain.Name = dto.Name;
+            domain.Classification = dto.Classification;
+            domain.BuildDate = dto.BuildDate;
+            domain.Crew = dto.Crew;
+            domain.Egienepower = dto.Egienepower;
+            domain.CreatedAt = dto.CreatedAt;
+            domain.ModifiedAt = dto.ModifiedAt;
+
+            //siia tuleb kood, mis salvetab domain
+            //objekti andmebaasi'
+            //tuleb kasutada repository'd, mis
+            //on defineeritud Core projektis
+            //konstruktori kaudu tuleb injectida repository
+
+            await _context.Spaceships.AddAsync(domain);
+            await _context.SaveChangesAsync();
+
+            return domain;
+
+
+        }
     }
 }
+
